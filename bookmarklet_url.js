1b3b643d487cd79653718ae6e40c2942ed07843f
grosbouddha <grosbouddha@gmail.com>
2011-08-04 00:18:46 +0200

    navlet domain object

--- bookmarklet_url.js
+++ bookmarklet_url.js
@@ -1,9 +1,10 @@
 
+//javascript:(function () {  var bootstrapNode = document.createElement("script"); bootstrapNode.src="http://navlet.googlecode.com/git/navlet.js";   document.getElementsByTagName('head')[0].appendChild(bootstrapNode);  })()
 
-javascript:(function
+javascript:(function () {
 
 var bootstrapNode = document.createElement("script");
-bootstrapNode.src="";
+bootstrapNode.src="http://navlet.googlecode.com/git/navlet.js";
 
 
 document.getElementsByTagName('head')[0].appendChild(bootstrapNode);
--- navlet.js
+++ navlet.js
@@ -1,1 +1,44 @@
-alert("loaded");
+
+(function() {
+
+	var navlet = function() {} (
+		
+		var supportNavigationTimingAPI;
+		
+		var displayNoSupportMessage = function () {
+			
+				alert("Your browser does not support the Navigation TIming API");
+		};
+		
+		var displayAPIInfo = function () {
+			
+				alert("Your browser does support the Navigation TIming API");
+		};
+		
+		return {
+			init: function() {
+			
+				supportNavigationTimingAPI = window.performance ? true : false ;
+				
+				if(supportNavigationTimingAPI) {
+					displayAPIInfo();
+				}
+				else {
+					displayNoSupportMessage();
+				}
+			}
+		}
+	);
+	var supportNavigationTimingAPI = window.performance ? true : false ;
+	
+	if(supportNavigationTimingAPI) {
+		displayAPIInfo();
+	}
+	else {
+		();
+	}
+	
+	var displayNoSupportMessage = function 
+	
+
+})()
