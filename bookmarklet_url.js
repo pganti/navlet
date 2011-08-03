6aae4ba3ddce2dd4d781ff96ad463cfa51a631bf
grosbouddha <grosbouddha@gmail.com>
2011-08-04 00:00:31 +0200

    asda

---
+++ bookmarklet_url.js
@@ -0,0 +1,11 @@
+
+
+javascript:(function
+
+var bootstrapNode = document.createElement("script");
+bootstrapNode.src="";
+
+
+document.getElementsByTagName('head')[0].appendChild(bootstrapNode);
+
+})()
