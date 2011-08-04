6263d97aac46913ed719d4b855958d9238422168
grosbouddha <grosbouddha>
2011-08-04 08:31:27 +0000

    Adding initial implementation for API dump display

--- navlet.js
+++ navlet.js
@@ -1,4 +1,3 @@
-
 (function() {
 
 	var navlet = (function() {
@@ -12,7 +11,59 @@
 		
 		var displayAPIInfo = function () {
 			
-				alert("Your browser support the Navigation TIming API");
+				var p = window.performance.timing;
+        
+        var mainContainerDiv = document.createElement("div");
+        mainContainerDiv.id = "mainContainer";
+        mainContainerDiv.setAttribute("style", "position:absolute;bottom:0; left:0; right:0;background-color:black;opacity:0.85;z-index:10000;");
+        
+        var advListDiv = document.createElement("div");
+        advListDiv.setAttribute("style", "float: left; padding-left:50px;");
+        advListDiv.id = "advList";
+        
+        var rawListDiv = document.createElement("div");
+        rawListDiv.setAttribute("style", "float: left;");
+        rawListDiv.id = 'rawList';
+        
+        var closeAnchor = document.createElement("a");
+        closeAnchor.id = 'closeAnchor';
+        closeAnchor.innerHTML = "close";
+        closeAnchor.setAttribute("style", "position:absolute;top:0; left:0; display:block; text-decoration:underline;color: yellow; padding: 10px; cursor:pointer;");
+        closeAnchor.setAttribute('onClick', 'javascript:document.getElementsByTagName("body")[0].removeChild(document.getElementById("mainContainer"))');
+        
+        document.getElementsByTagName('body')[0].appendChild(mainContainerDiv);
+        mainContainerDiv.appendChild(advListDiv);
+        mainContainerDiv.appendChild(rawListDiv);
+        mainContainerDiv.appendChild(closeAnchor);
+        
+        var rawList = "<ul>";
+        for(var key in p) {
+           rawList += "<li style='color:white'><span style='font-weight: bold;'>"+key+"</span>: "+p[key] + "</li>"
+        }
+        rawList += "</ul>";
+        
+        document.getElementById('rawList').innerHTML = rawList;
+        
+        var advList = "<ul>";
+        
+        function addItemToAdvList(key, value) {
+            advList += "<li style='color:white'><span style='font-weight: bold;'>"+key+"</span>: "+value + "</li>";
+        }
+        
+        addItemToAdvList("responseStart - navigationTime", (p.responseStart - p.navigationStart));
+        
+        addItemToAdvList("domLoading - responseStart", (p.domLoading - p.responseStart));
+        addItemToAdvList("domInteractive - responseStart", (p.domInteractive - p.responseStart));
+        
+        addItemToAdvList("domContentLoadedEventStart - responseStart", (p.domContentLoadedEventStart - p.responseStart));
+        addItemToAdvList("domReadyEvent duration", (p.domContentLoadedEventEnd - p.domContentLoadedEventStart));
+        
+        addItemToAdvList("loadEventStart - responseStart", (p.loadEventStart - p.responseStart));
+        addItemToAdvList("loadEvent duration", (p.loadEventEnd - p.loadEventStart));
+        
+        advList += "</ul>";
+        
+        document.getElementById('advList').innerHTML = advList;
 		};
 		
 		return {
